8d551fd3c7996d039a256fb0a9edcff02ded984d
usha <ushabajgain48@gmail.com>
2025-07-27 11:41:49 +0545

    New model Category and product implemented

--- blog_app/models.py
+++ blog_app/models.py
@@ -5,3 +5,18 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=100, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
